3357302cc1b6bd05b0f1bbdb7b03d345edb7a1cf
IAMDOGG <124060531+IAMDOGG@users.noreply.github.com>
2023-03-06 01:10:10 +0800

    Add files via upload

---
+++ ROSELL_Conversion/ROSELL_Conversion/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ROSELL_Conversion
+{
+    internal class Conversion
+
+    {
+        static void Main(string[] args)
+        {
+             
+
+        Console.Write("Enter length (in centimeters) >>>");
+        double lengthInCm = double.Parse(Console.ReadLine());
+
+        double lengthInInches = Math.Round(lengthInCm / 2.54);
+        int yards = (int)(lengthInInches / 36);
+        lengthInInches -= yards * 36;
+        int feet = (int)(lengthInInches / 12);
+        lengthInInches -= feet * 12;
+        int inches = (int)lengthInInches;
+
+        Console.WriteLine($"{yards} yards, {feet} feet{(feet == 1 ? "" : "s")}, {inches} inches");
+        }
+    }
+}
